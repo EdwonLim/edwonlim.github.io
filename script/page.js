0b66a398c78d521b1d9d366d06f886c00a8c768c
EdwonLim <edwon.lim@gmail.com>
2014-01-06 21:53:53 +0800

    update

--- script/page.js
+++ script/page.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
     var fix_flag = false;
+    var pos = $('.inner h3.menu').position().top + 20;
     setInterval(function() {
         var t = document.documentElement.scrollTop || document.body.scrollTop;
-        if ((t >= 227) != fix_flag) {
-            fix_flag = (t >= 227);
+        if ((t >= pos) != fix_flag) {
+            fix_flag = (t >= pos);
             if (fix_flag) {
                 $('.inner').removeClass('nofix').addClass('fix');
             } else {
                 $('.inner').removeClass('fix').addClass('nofix');
             }
         }
-    }, 200);
+    }, 1000 / 60);
 });
